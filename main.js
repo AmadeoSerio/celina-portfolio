4eabcfa67e19137febbb250fdd796b49c614cd86
Amadeo Serio <amadeoserio@gmail.com>
2023-08-12 18:38:03 -0300

    agregué un favicon, mejoré el head, corregí las mediasqueries y trabajé con JS arreglos, clases y filtros.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/*
+
 let casamiento = 20000;
 let cumpleaños = 20000;
 let producto = 10000;
@@ -43,6 +45,8 @@ while (pedidoDeTrabajo != "esc") {
     pedidoDeTrabajo = prompt("Ingrese el tipo de servicio de fotografía que quiere relizar (casamiento, cumpleaños, producto, recitales, emprendimiento)").toLowerCase();
 }
 
+*/
+
 // CONDICIONAL QUE FUNCIONA EN EL LIVE SERVER ¡¡SÓLO EN LA PESTAÑA DE DE CONTACTO!!
 
 /*
@@ -66,6 +70,8 @@ else {
 }*/
 
 
+
+/*
 // FUNCIÓN PARA SUMAR SERVICIOS DE FOTOGRAFÍAS  *****RECORDAR COMENTAR EL WHILE PARA QUE FUNCIONE*****
 function sumar(num1, num2) {
     return num1 + num2;
@@ -86,3 +92,82 @@ const portcentaje = (video) => video * 0.75;
 // Sólo hay que modificar donde dice suma acá debajo por el servicio deseado para calcular.
 let precioFinal = resta(suma(recitales, portcentaje(video)), promo);
 console.log(precioFinal);
+
+*/
+
+class Fotografia {
+    constructor(nombre, precio) {
+      this.nombre = nombre;
+      this.precio = precio;
+      this.vendido = false;
+    }
+  
+    fechaOcupada() {
+      this.vendido = true;
+    }
+  }
+  
+  const eventos = [];
+  
+  eventos.push(new Fotografia("casamiento", 20000));
+  eventos.push(new Fotografia("cumpleaño", 20000));
+  eventos.push(new Fotografia("producto", 10000));
+  eventos.push(new Fotografia("recital", 18000));
+  eventos.push(new Fotografia("emprendimiento", 12000));
+  eventos.push(new Fotografia("video", 18000));
+  
+  let nombre = prompt("Ingrese el tipo de servicio de fotografía que quiere relizar (casamiento, cumpleaño, producto, recital, emprendimiento o video). En caso de que quieras cerrar la ventana escriba: 'esc'").toLowerCase();
+  
+
+  while (nombre != "esc") {
+    const servicio = eventos.find((item) => item.nombre === nombre);
+    
+  
+    if (servicio) {
+      let mensaje = `
+        Nombre: ${servicio.nombre}
+        Precio: ${servicio.precio}
+      `;
+  
+      alert(mensaje);
+    } else {
+      alert("El servicio ingresado es incorrecto");
+    }
+ 
+    nombre = prompt("Ingrese el tipo de servicio de fotografía que quiere relizar (casamiento, cumpleaño, producto, recital, emprendimiento o video). En caso de que quieras cerrar la ventana escriba: 'esc'");
+  }
+
+      /*
+    const filtrado = eventos.filter((item) => item.precio > precio);
+     filtrado.forEach((servicio) => {
+       let mensaje =  `
+       Nombre: ${servicio.nombre}
+       Precio: ${servicio.precio}
+     `;
+     alert(mensaje);
+     }  
+     )
+*/
+//ESTÁ HECHO CON UN FOR
+  // while (nombre != "esc") {
+  //   let servicio;
+  
+  //   for (const item of eventos) {
+  //     if (item.nombre === nombre) {
+  //       servicio = item;
+  //     }
+  //   }
+  
+  //   if (servicio) {
+  //     let mensaje = `
+  //       Nombre: ${servicio.nombre}
+  //       Precio: ${servicio.precio}
+  //     `;
+  
+  //     alert(mensaje);
+  //   } else {
+  //     alert("No hay fecha para realizar el trabajo");
+  //   }
+  
+  //   nombre = prompt("Ingrese el tipo de servicio de fotografía que quiere relizar (casamiento, cumpleaños, producto, recitales, emprendimiento o videos). En caso de que quieras cerrar la ventana escriba: 'esc'");
+  // }
